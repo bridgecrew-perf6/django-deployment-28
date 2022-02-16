06fd8cb7e0558068e9cd4622e5e236f62d926c78
mushaid01 <mir786mushaid@gmail.com>
2022-02-16 07:27:18 -0800

    first commit

---
+++ learning_templates/basic_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BasiAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'basi_app'
---
+++ learning_templates/basic_app/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path,include
+from .import views
+
+
+app_name='basic_app'
+urlpatterns = [
+    path('',views.index,name="index"),
+    path('other/',views.other,name="other"),
+    path('relative/',views.relative,name="relative")
+]
